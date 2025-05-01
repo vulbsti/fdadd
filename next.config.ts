3adbd3fbf8495b590ed2ce145cf7e3271b0aa8f6
Utkarsh Gupta <utkarshgupta4321@gmail.com>
2025-05-01 10:43:40 +0000

    make the code deployable on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    // Temporarily removing this to surface build errors
-    // ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
